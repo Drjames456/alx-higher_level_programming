1daa95df52467aa694f0e3c75f974c714ce35137
Osunba Adeola <osunbaadeola56@gmail.com>
2024-01-12 06:23:25 -0800

    class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,20 +4,11 @@
 
 class Square:
     """ An empty class"""
-    def __init__(self, size = 0):
-        self.__size = size
-        self.size = 0
+    def __init__(self, size=0):
 
-
-
-
-    def size(self):
-        return self.__size
-
-    def size(self, value):
-        if not isinstance(value, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self.__size = size
