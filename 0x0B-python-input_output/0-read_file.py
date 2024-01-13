e89d4870d8a6503b0a099d7aecda44da466aaa56
Osunba Adeola <osunbaadeola56@gmail.com>
2024-01-12 23:58:43 -0800

    File handling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""A funtion that reads a file and print it to stdout"""
+
+
 def read_file(filename=""):
+    """ print the content of UTF* to stdout"""
+
     with open("UTF8.txt", "r")
     content = file.read()
     print(content)
