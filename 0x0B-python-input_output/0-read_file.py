72b9061d3f23e9cd5faf5abb07e32428d3ebd414
Osunba Adeola <osunbaadeola56@gmail.com>
2024-01-12 22:33:47 -0800

    File handling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, my_file = "UTF8") as  file:
+    with open(filename, encoding = "UTF8") as file:
         for line in file:
             print(line, end = "")
