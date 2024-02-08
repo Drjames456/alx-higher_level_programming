b7df5b59301adf3ee78ec4b26f5e1fb152551ce9
Osunba Adeola <osunbaadeola56@gmail.com>
2024-02-08 12:12:05 -0800

    More classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""this is a module that defines an empty class"""
+"""this is a module that defines a class Rectangle"""
 
 
 class Rectangle:
     """this define a class Rectangle"""
+
     def __init__(self, width = 0, height = 0):
         """this function define the dimension of a rectangle"""
+
         self.width = width
         self.height = height
 
     def width(self):
         """define the width of the rectangle"""
+
         return self.__width
 
     def width(self, value):
         """to get the value of width of the rectangle"""
+
         if not isinstance(value, int):
             raise TypeError('value must be integer')
         if value < 0:
@@ -23,10 +27,12 @@ class Rectangle:
 
     def height(self):
         """define the height of rectangle"""
+
         return self.__height
 
     def height(self, value):
         """To get the height of a rectangle"""
+
         if not isintance(value, int):
             raise TypeError('value must be integer')
         if value < 0:
@@ -35,10 +41,12 @@ class Rectangle:
 
     def area(self):
         """get the area of the rectangle"""
+
         return self.__width * self.__height
 
     def perimeter(self):
         """to get the perimeter of the rectangle"""
+
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
