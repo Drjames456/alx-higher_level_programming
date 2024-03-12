3c5b3dc1d63f72045ad6a29eecf98084565a7e1b
Osunba Adeola <osunbaadeola56@gmail.com>
2024-03-11 17:52:06 -0700

    Js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < messages.length; i++) {
+  output += messages[i] + (i < messages.length - 1 ? '\n' : '');
+}
+
+console.log(output);
