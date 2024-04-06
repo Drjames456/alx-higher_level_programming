9104f2ee211a0bc1c9f8653b20a338bf1bb8d9c8
Osunba Adeola <osunbaadeola56@gmail.com>
2024-04-06 13:56:23 -0700

    If/else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for number in range(100):
-    if number < 99:
-        print(f"{number:02d}, ", end="")
-    else:
-        print(f"{number:02d}")
+    end_char = ", " if number < 99 else "\n"
+    print(f"{number:02d}{end_char}", end="")
