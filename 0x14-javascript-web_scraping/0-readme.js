78afad723b244310d40a8ad7147f708ae1bf9b60
Osunba Adeola <osunbaadeola56@gmail.com>
2024-04-23 12:48:22 -0700

    Javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readAndPrintFile(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(data);
-    });
+function readAndPrintFile (filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
 }
 
 const filePath = process.argv[2];
 if (!filePath) {
-    console.error("Usage: node script.js <file_path>");
-    process.exit(1);
+  console.error('Usage: node script.js <file_path>');
+  process.exit(1);
 }
 
 readAndPrintFile(filePath);
-
-
