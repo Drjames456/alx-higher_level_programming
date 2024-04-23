e5954d85f261088b8c28f5f012acf25c73b7dcb0
Osunba Adeola <osunbaadeola56@gmail.com>
2024-04-23 13:08:24 -0700

    Js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-function writeStringToFile(filePath, content) {
-    fs.writeFile(filePath, content, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-            return;
-        }
-        console.log('File has been written successfully.');
-    });
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-if (!filePath || !content) {
-    console.error('Usage: node script.js <file_path> <string_to_write>');
-    process.exit(1);
-}
-
-writeStringToFile(filePath, content);
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.error(err);
+  }
+});
